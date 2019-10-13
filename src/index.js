a79752ffab8de06e9f19e2a5b19e6aaf10ee0efd
Aristotle The Axolotl <ttab1998@gmail.com>
2019-10-13 18:16:59 +0100

    seperating from the-philosophers-components

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/extensions */
+import { Proxy } from './proxy';
+import { Resource } from './resource';
+import { Server } from './server';
+import { Session } from './session';
+
+module.exports = {
+  Proxy,
+  Resource,
+  Server,
+  Session,
+};
